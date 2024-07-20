ab7f66ace66a8b2f32b95edb30d043e9b92a1782
Antonio Masotti <toniomasotti@gmail.com>
2024-07-20 15:14:37 +0200

    docs: add documentation strings

--- src/check_cidrs.rs
+++ src/check_cidrs.rs
@@ -2,9 +2,19 @@ use aws_sdk_ec2::Client as Ec2Client;
 use aws_sdk_ec2::operation::describe_security_groups::{DescribeSecurityGroupsOutput};
 use aws_sdk_ec2::types::Filter;
 
+/// Default value for the CIDR to search
 const DEFAULT_ROUTE_VALUE: &str = "0.0.0.0/0";
 
-pub async fn find_sg_default_ingress(ec2client: Ec2Client) {
+/// Find the security groups with too wide ingress rules
+///
+/// # Arguments
+///
+/// * `ec2_client` - The EC2 client to use
+///
+/// # Returns
+///
+/// Nothing, it prints the security groups with too wide ingress rules in the console
+pub async fn find_sg_default_ingress(ec2_client: Ec2Client) {
 
 
     let filter_ingress = Filter::builder()
@@ -12,7 +22,7 @@ pub async fn find_sg_default_ingress(ec2client: Ec2Client) {
         .values(DEFAULT_ROUTE_VALUE)
         .build();
 
-    let sg_descr = ec2client
+    let sg_descr = ec2_client
         .describe_security_groups()
         .set_filters(Some(vec![filter_ingress]))
         .send()
@@ -22,13 +32,22 @@ pub async fn find_sg_default_ingress(ec2client: Ec2Client) {
     print_sg_description(sg_descr)
 }
 
-pub async fn find_sg_default_egress(ec2client: Ec2Client) {
+/// Find the security groups with too wide egress rules
+///
+/// # Arguments
+///
+/// * `ec2_client` - The EC2 client to use
+///
+/// # Returns
+///
+/// Nothing, it prints the security groups with too wide egress rules in the console
+pub async fn find_sg_default_egress(ec2_client: Ec2Client) {
     let filter_egress = Filter::builder()
         .name("egress.ip-permission.cidr")
         .values(DEFAULT_ROUTE_VALUE)
         .build();
 
-    let sg_descr = ec2client
+    let sg_descr = ec2_client
         .describe_security_groups()
         .set_filters(Some(vec![filter_egress]))
         .send()
@@ -38,13 +57,22 @@ pub async fn find_sg_default_egress(ec2client: Ec2Client) {
     print_sg_description(sg_descr)
 }
 
-pub async fn find_sg_all_ports(ec2client: Ec2Client) {
+/// Find the security groups with all ports open
+///
+/// # Arguments
+///
+/// * `ec2_client` - The EC2 client to use
+///
+/// # Returns
+///
+/// Nothing, it prints the security groups with all ports open in the console
+pub async fn find_sg_all_ports(ec2_client: Ec2Client) {
     let filter_all_ports = Filter::builder()
         .name("ip-permission.to-port")
         .set_values(Some(vec!["-1".to_string(), "0".to_string()]))
         .build();
 
-    let sg_descr = ec2client
+    let sg_descr = ec2_client
         .describe_security_groups()
         .set_filters(Some(vec![filter_all_ports]))
         .send()
@@ -54,7 +82,11 @@ pub async fn find_sg_all_ports(ec2client: Ec2Client) {
     print_sg_description(sg_descr)
 }
 
-
+/// Describe all the security groups
+///
+/// # Arguments
+///
+/// * `ec2_client` - The EC2 client to use
 pub async fn describe_all_sg(ec2_client: Ec2Client) {
     let sg_descr = ec2_client
         .describe_security_groups()
@@ -65,6 +97,46 @@ pub async fn describe_all_sg(ec2_client: Ec2Client) {
     print_sg_description(sg_descr)
 }
 
+/// Print the security group description
+///
+/// # Arguments
+///
+/// * `sg_desc` - The security group description (output of the describe_security_groups operation)
+///
+/// # Returns
+///
+/// Nothing, it prints the security group description in the console in this format:
+///
+///```text
+/// Security Group: "typo3-security-group"
+///
+/// Security Group: "my-security-group"
+///         Description: "My security group"
+///         ID: "sg-1234567890abcdef0"
+///         VPC ID: "vpc-12345678"
+/// Inbound rules:
+///         From port: 80
+///         To port: 80
+///         Protocol: "tcp"
+///         IP Range: IpRange { cidr_ip: Some("0.0.0.0/0"), description: None }
+///                 CIDR: Some("0.0.0.0/0")
+///         From port: 8025
+///         To port: 8025
+///         Protocol: "tcp"
+///         IP Range: IpRange { cidr_ip: Some("0.0.0.0/0"), description: Some("Service") }
+///                 CIDR: Some("0.0.0.0/0")
+///         From port: 22
+///         To port: 22
+///         Protocol: "tcp"
+/// Outbound rules:
+///         From port: -1
+///         To port: -1
+///         Protocol: "-1"
+///         IP Range: IpRange { cidr_ip: Some("0.0.0.0/0"), description: None }
+///                 CIDR: Some("0.0.0.0/0")
+/// ```
+///
+///
 fn print_sg_description(sg_desc: DescribeSecurityGroupsOutput) {
     sg_desc.security_groups.unwrap().iter().for_each(|sg| {
         if let Some(group_name) = &sg.group_name {
--- src/list_sg.rs
+++ src/list_sg.rs
@@ -1,7 +1,12 @@
 use aws_sdk_ec2::Client as Ec2Client;
 
-pub async fn list_security_groups(client: &Ec2Client) {
-    let response = client.describe_security_groups().send().await.unwrap();
+/// List the security groups in the AWS account by name
+///
+/// # Arguments
+///
+/// * `ec2_client` - The EC2 client to use
+pub async fn list_security_groups(ec2_client: &Ec2Client) {
+    let response = ec2_client.describe_security_groups().send().await.unwrap();
     let security_groups = response.security_groups.unwrap();
     for sg in security_groups {
         println!("Security Group: {:?}", sg.group_name);
